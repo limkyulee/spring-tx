b20da672b0ca571145d4aaf7e0c4b62d0fa0f7a0
limkyulee <mud0308@gmail.com>
2025-06-01 21:25:57 +0900

    Initial commit

---
+++ src/test/java/hello/spring_tx/SpringTxApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.spring_tx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
