3d8ac7753549bd7daa6f8ea7f72dbc04dcd66f65
kyuleelim <mud0308@gmail.com>
2025-06-04 22:39:44 +0900

    hikari connection ex

--- src/test/java/hello/spring_tx/propagation/BasicTxTest.java
+++ src/test/java/hello/spring_tx/propagation/BasicTxTest.java
@@ -59,6 +59,43 @@ public class BasicTxTest {
         log.info("트랜잭션 롤백 시작");
         transactionManager.rollback(status);
         log.info("트랜잭션 롤백 완료");
+    }
 
+    /**
+     * 커낵션 풀 사용에 의해 두 트랜잭션은 같은 0번 커넥션을 사용한다.
+     * 하지만, 트랜잭션1이 반남을 완료한 커넥션을 트랜잭션2가 획득한 것이기 때문에
+     * 둘은 완전히 다른 커넥션으로 인지하는 것이 맞다.
+     * 구분법 | 히카리 커넥션 풀에서 커넥션 획득 시,
+     *       | 실제 커넥션 반환이 아닌 '내부 관리를 위한 히카리 커넥션 객체를 생성하여 반환'한다.
+     *       | 내부에 실제 물리 커넥션이 포함되어있긴함.
+     *       | 히카리 커넥션풀이 반환해주는 커넥션이 다루는 프록시 객체 주소를 확인하면 구분이 가능하다. (다름)
+     */
+    @Test
+    void double_commit(){
+        log.info("트랜잭션1 시작");
+        TransactionStatus status1 = transactionManager.getTransaction(new DefaultTransactionDefinition());
+        log.info("트랜잭션1 커밋");
+        transactionManager.commit(status1);
+
+        log.info("트랜잭션2 시작");
+        TransactionStatus status2 = transactionManager.getTransaction(new DefaultTransactionDefinition());
+        log.info("트랜잭션2 커밋");
+        transactionManager.commit(status2);
+    }
+
+    /**
+     * 서로 다른 커넥션을 사용하기 때문에 각각의 커밋과 롤백에 영향을 주지않는다.
+     */
+    @Test
+    void double_commit_rollback(){
+        log.info("트랜잭션1 시작");
+        TransactionStatus status1 = transactionManager.getTransaction(new DefaultTransactionDefinition());
+        log.info("트랜잭션1 커밋");
+        transactionManager.commit(status1);
+
+        log.info("트랜잭션2 시작");
+        TransactionStatus status2 = transactionManager.getTransaction(new DefaultTransactionDefinition());
+        log.info("트랜잭션2 롤백");
+        transactionManager.rollback(status2);
     }
 }
